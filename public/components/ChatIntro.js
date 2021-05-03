a00f8dd059dcd8d993386a58e78572c6c682bde0
MarlonDener <m.dener01@hotmail.com>
2021-05-02 22:15:46 -0300

    Add Interface chat

--- public/components/ChatIntro.js
+++ public/components/ChatIntro.js
@@ -6,7 +6,7 @@ export default () => {
         <div className={styles.chatIntro}>
             <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt="Intro Whatsapp" />
             <h1>Mantenha seu celular conectado</h1>
-            <h2>O whtasApp conecta seu telefone para sincronizar sas mensagens.<br />Para reduzir o uso de dadosm conecte sue telefone  auma rede Wifi</h2>
+            <h2>O whtasApp conecta seu telefone para sincronizar suas mensagens .<br />Para reduzir o uso de dadosm conecte sue telefone  auma rede Wifi</h2>
         </div>
     );
 }
